19d5c3e410997f707a9bbaecec71f12d20868458
Rafi5734 <kamrulhasun5734@gmail.com>
2024-02-22 14:07:37 +0600

    latest news content updated

--- src/pages/latestNews/latestNewsList/LatestNewsList.jsx
+++ src/pages/latestNews/latestNewsList/LatestNewsList.jsx
@@ -44,31 +44,33 @@ const LatestNewsList = () => {
               </Link>
             </div>
           </Col>
-          {/* <Col xs={12} md={6} lg={3} className="mt-3">
+          <Col xs={12} md={6} lg={3} className="mt-3">
             <div data-aos="fade-right">
               <Link to="#" className="text-decoration-none">
                 <Card style={{ width: "100%" }}>
                   <Card.Body>
                     <Card.Title className="d-flex justify-content-center">
                       <img
-                        src="https://demo.phpscriptpoint.com/travelfresh/public/uploads/team-member-5-thumb.jpg"
+                        src="https://i.ibb.co/BGt73Jb/Whats-App-Image-2024-02-22-at-1-15-24-PM.jpg"
                         alt="employee_img"
                         className="img-fluid"
                       />
                     </Card.Title>
                     <Card.Subtitle className="mb-2 text-muted text-center fs-3 ">
                       <p className="text-decoration-none text-reset">
-                        International Tour
+                        আরেকজন নিয়োগপ্রাপ্ত ক্লায়েন্ট
                       </p>
                     </Card.Subtitle>
                     <Card.Text className="">
                       <p className="text-decoration-none text-reset">
-                        18 Feb, 2024
+                        22 Feb, 2024
                       </p>
                       <p className="text-dark">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Illum temporibus aut commodi nobis, exercitationem
-                        aperiam facilis minus cumque cum repellat.
+                        ইব্রাহিম দর্জি , উনি আজকে আমাদের মাধ্যমে কল্যাণপুর একটি
+                        এপার্টমেন্টে সিকিউরিটি জব এ জয়েন করেন। ইব্রাহিম রহমান
+                        সাহেব চাকরিটি পেয়ে আমাদের প্রতি কৃতজ্ঞতা প্রকাশ করেছেন।
+                        উনি আমাদের ডিরেক্টর স্যারের কাছ থেকে প্রশংসা পত্র গ্রহণ
+                        করেন।
                       </p>
                     </Card.Text>
                     <div>
@@ -79,7 +81,7 @@ const LatestNewsList = () => {
               </Link>
             </div>
           </Col>
-          <Col xs={12} md={6} lg={3} className="mt-3">
+          {/* <Col xs={12} md={6} lg={3} className="mt-3">
             <div data-aos="fade-right">
               <Link to="#" className="text-decoration-none">
                 <Card style={{ width: "100%" }}>
